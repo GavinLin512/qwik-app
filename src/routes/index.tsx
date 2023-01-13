a523e1be26f597b8dd44ebb817b3ec9839c97a3a
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2023-01-13 11:20:10 +0800

    test auto deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
   return (
     <div>
       <h1>
-        Welcome to Qwik 123 <span class="lightning">⚡️</span>
+        Welcome to Qwik 123456<span class="lightning">⚡️</span>
       </h1>
 
       <ul>
